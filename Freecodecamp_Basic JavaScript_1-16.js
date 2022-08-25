f05c032e0a038fd5c8ff56f1a3537ae46b716837
ValeriyK <106479316+ValeriyKan@users.noreply.github.com>
2022-08-25 22:02:03 +0400

    add file

---
+++ Freecodecamp_Basic JavaScript_1-16.js
@@ -0,0 +1,68 @@
+//Comment Your JavaScript Code
+// my comment.
+/* my comment */
+
+//Declare JavaScript Variables
+var myName;
+
+// Storing Values with the Assignment Operator
+var a;
+a = 7;
+
+// Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b = a;
+
+// Initializing Variables with the Assignment Operator
+var a = 9;
+
+// Declare String Variables
+var myFirstName = "Valeriy";
+var myLastName = "Kanaev";
+
+//Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Understanding Case Sensitivity in Variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Declare a Read-Only Variable with the const Keyword
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//Add Two Numbers with JavaScript
+const sum = 10 + 10;
+
+// Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+
+//Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+//Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+//Increment a Number with JavaScript
+let myVar = 87;
+myVar++;
+
+//Decrement a Number with JavaScript
+let myVar = 11;
+myVar--;
