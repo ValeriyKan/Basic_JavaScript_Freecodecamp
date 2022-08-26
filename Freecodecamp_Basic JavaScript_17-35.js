197274cfaa4d75aeae54985a06ade8300941ac19
ValeriyK <106479316+ValeriyKan@users.noreply.github.com>
2022-08-26 22:30:02 +0400

    add file

---
+++ Freecodecamp_Basic JavaScript_17-35.js
@@ -0,0 +1,91 @@
+// Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//let a = 11;
+let b = 9;
+let c = 3;
+
+//Compound Assignment With Augmented Subtraction
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+// Change this line
+
+//Quoting Strings with Single Quotes
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>'; 
+
+//Escape Sequences in Strings
+const myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
+
+// Concatenating Strings with Plus Operator
+const myStr ="This is the start. "+"This is the end."; // Change this line
+
+//Concatenating Strings with the Plus Equals Operator
+let myStr = "This is the first sentence. ";
+myStr += 'This is the second sentence.';
+
+
+//Constructing Strings with Variables
+// Only change code below this line
+const myName = "Valeriy";
+const myStr = "My name is" + myName + "and I am well!";
+
+//Appending Variables to Strings
+// Change code below this line
+const someAdjective = "acd";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//Use Bracket Notation to Find the First Character in a String
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; 
+
+//Understand String Immutability
+let myStr = "Jello World";
+myStr = "Hello World"; 
+
+//Use Bracket Notation to Find the Last Character in a String
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
